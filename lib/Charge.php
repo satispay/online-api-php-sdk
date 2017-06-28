d65f1aee3299717a27fe3b1996eae8455feacd8d
Alessio Dionisi <me@alessiodionisi.com>
2017-06-28 17:40:07 +0200

    Checkout API

--- lib/Charge.php
+++ lib/Charge.php
@@ -3,7 +3,21 @@ namespace SatispayOnline;
 
 use SatispayOnline\Api;
 
+/**
+ * @property string $user_id
+ * @property string $description
+ * @property string $currency
+ * @property int    $amount
+ * @property array  $metadata
+ * @property bool   $required_success_email
+ * @property int    $expire_in
+ * @property string $callback_url
+ */
 class Charge {
+  /**
+   * @param  array  $params
+   * @return object
+   */
   public static function create($params = null) {
     $result = Api::request('/online/v1/charges', 'POST', $params);
     $body = $result['body'];
