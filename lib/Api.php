f0f278abb6d8827d3df9b24fb34654d726a41f21
Alessio Dionisi <hello@adns.io>
2017-08-29 18:38:45 +0200

    bump version

--- lib/Api.php
+++ lib/Api.php
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace SatispayOnline;
 
-define('SDKVERSION', '1.3.2');
+define('SDKVERSION', '1.3.3');
 
 class Api {
   public static $securityBearer;
