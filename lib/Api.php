6b6a5972b3e7af9ff902f30f69e3a461c61ae10d
Alessio Dionisi <hello@adns.io>
2017-10-16 11:16:03 +0200

    fixed comments

--- lib/Api.php
+++ lib/Api.php
@@ -1,21 +1,21 @@
 <?php
-/*
-Satispay Online API PHP SDK
-Copyright (C) 2017  Satispay
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/** 
+ * Satispay Online API PHP SDK
+ * Copyright (C) 2017  Satispay
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace SatispayOnline;
 
@@ -25,7 +25,7 @@ class Api {
   public static $endpoint = 'https://authservices.satispay.com';
   public static $staging = false;
   public static $client = null;
-  public static $version = '1.3.4';
+  public static $version = '1.3.5';
 
   public static function setSecurityBearer($securityBearer) {
     self::$securityBearer = $securityBearer;
