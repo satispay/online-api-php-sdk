d656f9c9babe7db9154a9162adc2a41e228cc18a
Alessio Dionisi <hello@adns.io>
2018-02-28 17:53:47 +0100

    require_once on init

--- init.php
+++ init.php
@@ -1,8 +1,8 @@
 <?php
-require(dirname(__FILE__) . '/lib/Api.php');
+require_once(dirname(__FILE__) . '/lib/Api.php');
 
-require(dirname(__FILE__) . '/lib/Bearer.php');
-require(dirname(__FILE__) . '/lib/Charge.php');
-require(dirname(__FILE__) . '/lib/Checkout.php');
-require(dirname(__FILE__) . '/lib/Refund.php');
-require(dirname(__FILE__) . '/lib/User.php');
+require_once(dirname(__FILE__) . '/lib/Bearer.php');
+require_once(dirname(__FILE__) . '/lib/Charge.php');
+require_once(dirname(__FILE__) . '/lib/Checkout.php');
+require_once(dirname(__FILE__) . '/lib/Refund.php');
+require_once(dirname(__FILE__) . '/lib/User.php');
